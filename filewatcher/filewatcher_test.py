963b3cbb82e74c3602effb15fb3929e64ed2d30c
Wojtek Erbetowski <wojtek@erbetowski.pl>
2018-12-12 12:40:10 +0100

    Testing file hashing

---
+++ filewatcher/filewatcher_test.py
@@ -0,0 +1,26 @@
+import os
+
+import pytest
+from click.testing import CliRunner
+
+from . import filewatcher
+
+
+@pytest.fixture
+def runner() -> CliRunner:
+    runner = CliRunner()
+
+    with runner.isolated_filesystem():
+        yield runner
+
+
+def test_integration(runner: CliRunner):
+    os.mkdir("workdir")
+
+    result = runner.invoke(
+        filewatcher.cli,
+        ["init", "-s", "log.json", "workdir"]
+    )
+
+    assert result.output == ""
+    assert result.exit_code == 0
