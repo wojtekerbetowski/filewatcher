f63b12902fcdd072d7547376a578519b5282034d
Wojtek Erbetowski <wojtek@erbetowski.pl>
2018-12-12 12:49:57 +0100

    Add bash/fish completion

--- filewatcher/filewatcher.py
+++ filewatcher/filewatcher.py
@@ -5,6 +5,7 @@ import os
 import pathlib
 
 import click
+import click_completion
 
 logger = logging.Logger("main")
 
@@ -76,6 +77,32 @@ def verify(base, store, hash_type):
     if diff:
         print(f"Files changed: {diff}")
         exit(len(diff))
+
+
+@cli.command()
+@click.option(
+    "--append/--overwrite", help="Append the completion code to the file", default=None
+)
+@click.option(
+    "-i", "--case-insensitive/--no-case-insensitive", help="Case insensitive completion"
+)
+@click.argument(
+    "shell",
+    required=False,
+    type=click_completion.DocumentedChoice(click_completion.core.shells),
+)
+@click.argument("path", required=False)
+def completion(append, case_insensitive, shell, path):
+    """Install the click-completion-command completion"""
+    extra_env = (
+        {"_CLICK_COMPLETION_COMMAND_CASE_INSENSITIVE_COMPLETE": "ON"}
+        if case_insensitive
+        else {}
+    )
+    shell, path = click_completion.core.install(
+        shell=shell, path=path, append=append, extra_env=extra_env
+    )
+    click.echo("%s completion installed in %s" % (shell, path))
 
 
 if __name__ == '__main__':
