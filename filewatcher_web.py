4fe06c821e89a66fc26bdb6b8d140afc6b0505dd
Wojtek Erbetowski <wojtek@erbetowski.pl>
2019-03-26 21:44:55 +0100

    Add verify web

--- filewatcher_web.py
+++ filewatcher_web.py
@@ -15,5 +15,27 @@ def hash_site():
     return flask.render_template('hashes.html', hash_tree=hash_tree)
 
 
+@app.route('/web/verify', methods=['GET', 'POST'])
+def verify_site():
+    if flask.request.method == 'GET':
+        return flask.render_template('verify_form.html')
+
+    directory = flask.request.form['directory']
+
+    hashes_to_verify = {}
+    for item in flask.request.form['hashes'].split('\r\n'):
+        path, hash = item.split('=')
+        hashes_to_verify[path] = hash
+
+    hash_tree = filewatcher.hash_tree(directory)
+
+    if hash_tree == hashes_to_verify:
+        message = 'Verification passed'
+    else:
+        message = 'Verification failed'
+
+    return flask.render_template('verify.html', message=message)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
